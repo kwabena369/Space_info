78c3d78ce40e0b3da4eeb2abddf13566a134604f
TexasStudio <68285748+kwabena369@users.noreply.github.com>
2024-11-03 17:07:10 +0000

    Update main.js

--- src/main.js
+++ src/main.js
@@ -4,5 +4,5 @@ import "leaflet/dist/leaflet.css";
 import App from './App.vue'
 //  here is the making use of the router there in the system
  import main_router from "./routers/router.js"
-
+// there is hope
 createApp(App).use(main_router).mount('#app')
